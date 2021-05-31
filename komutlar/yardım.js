3d36de66f93bd12e63832c71fc6caf26fc4afe9e
fu-w <74312970+fu-w@users.noreply.github.com>
2021-05-31 16:02:25 +0300

    Create yardım.js

---
+++ komutlar/yardım.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+
+
+exports.run = (client, message, params) => {
+  
+  const embed = new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  
+  .setTitle("SANCTUS YARDIM MENÜSÜ")
+  .addField(".**komut**", `kendiniz yazınız`)
+  .addField(".**komut**", `kendiniz yazınız`)
+  .addField(".**komut**", `kendiniz yazınız`)
+  .addField(".**komut**", `kendiniz yazınız`)
+  .addField(".**komut**", `kendiniz yazınız`)
+  .addField(".**komut**", `kendiniz yazınız`)
+  .addField(".**komut**", `kendiniz yazınız`)
+  .addField(".**komut**", `kendiniz yazınız`)
+  
+message.channel.send(embed)
+}
+
+
+
+exports.conf = {//sanctus
+  enable: true,
+  guildOnly: false,
+  aliases: ["y"],
+  permLevel: 0
+}
+exports.help = {
+  name: "yardım",
+  description: "botun yardım menüsünü gösterir.",
+  usage: "yardım"
+}//sanctus
